9466296c6ada993fa137b130663c8e60399c943c
Anthony Wong <awong6201@gmail.com>
2019-11-28 00:53:17 -0800

    Darkened level, made enemy not shoot through walls

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,9 @@ public class Enemy : Character
     enum State
     {
         Idle,
-        Moving
+        Moving,
+        Alert,
+        Combat
     }
 
     // Reference to the pathfinder component.
@@ -19,33 +21,63 @@ public class Enemy : Character
     // Current state of the enemy agent.
     private State m_currentState;
 
+    // Which way should the enemy agent be looking towards?
+    private Vector3 m_targetDirection;
+
+    // Array of visible colliders for this enemy agent.
+    private Collider[] m_visibleColliders;
+
     private void Start()
     {
         m_currentState = State.Idle;
+        m_visibleColliders = new Collider[10];
     }
 
     private void Update()
     {
-        switch(m_currentState)
+        HandleStates();
+
+        // Get all nearby colliders, ignoring level layer.
+        Physics.OverlapSphereNonAlloc(transform.position, 5.0f, m_visibleColliders, ~LayerMask.GetMask("Level"));
+        foreach (Collider c in m_visibleColliders)
         {
-            case State.Idle:
-                //pathfinder.currentPath = pathfinder.CalculatePath(Level.TileAt(1, 5));
-                //m_currentState = State.Moving;
-                Collider[] colliders = Physics.OverlapSphere(transform.position, 5.0f);
-                foreach (Collider c in colliders)
+            if (c == null) { continue; }
+
+            Character character = c.GetComponent<Character>();
+            if (character != null && character.team != team)
+            {
+                Vector3 direction = character.transform.position - transform.position;
+
+                // Flatten and normalize the direction.
+                direction.y = 0f;
+                direction = Vector3.Normalize(direction);
+
+                // Check if we can actually see the target.
+                bool isTargetVisible = false;
+                Ray ray = new Ray(transform.position, direction);
+                if (Physics.Raycast(ray, out RaycastHit hit))
                 {
-                    Character character = c.GetComponent<Character>();
-                    if (character != null && character.team != team)
+                    if (hit.transform == character.transform)
                     {
-                        Vector3 direction = character.transform.position - transform.position;
-
-                        // Flatten and normalize the direction.
-                        direction.y = 0f;
-                        direction = Vector3.Normalize(direction);
-
-                        gun.Fire(direction);
+                        isTargetVisible = true;
                     }
                 }
+
+                if (isTargetVisible)
+                {
+                    gun.Fire(direction);
+                }
+            }
+        }
+    }
+
+    private void HandleStates()
+    {
+        switch (m_currentState)
+        {
+            case State.Idle:
+                pathfinder.currentPath = pathfinder.CalculatePath(Level.TileAt(1, 5));
+                m_currentState = State.Moving;
                 break;
             case State.Moving:
 
